3ec58185b9f4832cc79d6bd437271d897928c8ea
koleh <kolehd@gmail.com>
2023-06-26 22:33:39 +0930

    Added test for not leap year 🔴

---
+++ tests/Example.elm
@@ -0,0 +1,24 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+import LeapYear exposing (isLeapYear)
+
+-- Write a function that returns true or false depending on whether its input integer is a leap year or not.
+-- 
+-- A leap year is defined as one that is divisible by 4,
+-- but is not otherwise divisible by 100 unless it is also divisible by 400.
+-- 
+-- For example, 2001 is a typical common year and 1996 is a typical leap year, whereas 1900 is an atypical common year and 2000 is an atypical leap year.
+
+suite : Test
+suite =
+    describe "Leap year"
+        [
+            test "Years not divisible by 4 are not leap years" <|
+            \_ ->
+                isLeapYear 5
+                |> Expect.equal False
+        ]
+
