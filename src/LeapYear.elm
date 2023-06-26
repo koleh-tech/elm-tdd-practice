e92bd638d13d8d35959ef8c77951675f16600777
koleh <kolehd@gmail.com>
2023-06-26 22:53:15 +0930

    Make pass 🍏

--- src/LeapYear.elm
+++ src/LeapYear.elm
@@ -4,6 +4,10 @@ isLeapYear : Int -> Bool
 isLeapYear year =
     case remainderBy 4 year of
         0 ->
-            remainderBy 100 year /= 0
+            case remainderBy 400 year of
+                0 ->
+                    True
+                _ ->
+                    remainderBy 100 year /= 0
         _ ->
             False
