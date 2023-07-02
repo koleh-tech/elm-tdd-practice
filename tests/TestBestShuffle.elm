b6de8f35c76b2dec260979d0f8afea4449f33958
koleh <kolehd@gmail.com>
2023-07-02 13:31:45 +0930

    Added case for first shuffled string

--- tests/TestBestShuffle.elm
+++ tests/TestBestShuffle.elm
@@ -69,5 +69,10 @@ maxStringDiffSuite =
             \_ ->
                 bestOutOfShuffledStrings "tree" ["eetr", "eter", "tere"]
                     |> Expect.equal "eetr"
+        , test "If several shuffled strings are just as shuffled, picks first one" <|
+            \_ ->
+                bestOutOfShuffledStrings "grrrrrr" ["rgrrrrr", "rrgrrrr", "rrrgrrr"]
+                    |> Expect.equal "rgrrrrr"
+
         ]
 
